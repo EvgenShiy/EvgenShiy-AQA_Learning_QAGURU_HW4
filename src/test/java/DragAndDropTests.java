9f70253ca50ac1d6414dd496e3be56edfb332a38
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-09-14 00:36:42 +0500

    added DragAndDropTests

---
+++ src/test/java/DragAndDropTests.java
@@ -0,0 +1,43 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTests {
+    @BeforeAll
+    public static void setUp() {
+        Configuration.browserSize = "1920x1080";  // Устанавливаем размер окна браузера
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false; // используется только для отладки тестов (установить значение true)
+        Configuration.baseUrl = "https://the-internet.herokuapp.com/";
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    public void ActionElementsTest() {
+
+        open("/drag_and_drop");
+
+        actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+
+        $("#column-a").shouldNotHave(text("A"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+
+    @Test
+    public void DragAndDropElementsTest() {
+
+        open("/drag_and_drop");
+
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+
+        $("#column-a").shouldNotHave(text("A"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
+
--- src/test/java/GithubEnterpriseTest.java
+++ src/test/java/GithubEnterpriseTest.java
@@ -1,11 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withTagAndText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GithubEnterpriseTest {
